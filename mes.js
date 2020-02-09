7a9acbc84fb8d8b4a0d14a33241db9e36264d0a8
Iveth Martin <ivethmartinsamayoa@gmail.com>
2020-02-09 07:02:46 -0600

    Added a new js

---
+++ mes.js
@@ -0,0 +1,27 @@
+function calcMes(mes = 10){
+  if(mes == 9 || mes == 10 || mes == 11){
+  console.log( mes + " es otoño");
+  }
+  else if(mes == 12 || mes == 1 || mes == 2){
+    console.log( mes + " es invierno");
+  }
+  else if(mes == 3 || mes == 4 || mes == 5){
+    console.log( mes + " es primavera");
+  }
+  
+  else if(mes == 6 || mes == 7 || mes == 8){
+    console.log( mes + " es verano");
+  }
+  else{
+    console.log( "No es un mes del año");
+  }
+}
+calcMes();
+calcMes(prompt("Ingrese"))
+
+var carnét= "20009051"
+var nombre= "Silvia Iveth Martin Samayoa"
+alert("Carnét: " + " " +carnét +" "+ "\n"+"Nombre: " +" " + nombre );
+  
+      
+        
